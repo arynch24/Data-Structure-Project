15224fafeec364b593241ad2c98ad577f73f6839
arynch24 <arynch24@gmail.com>
2024-09-15 23:37:17 +0530

    changes made

--- visualizer/src/main/Main.java
+++ visualizer/src/main/Main.java
@@ -5,31 +5,5 @@ import gui.LinkedListGui;
 public class Main {
     public static void main(String[] args) {
         new LinkedListGui().setVisible(true);
-//        SwingUtilities.invokeLater(() -> {
-//            String[] options = {"Stack", "Queue", "LinkedList", "Deque"};
-//            String choice = (String) JOptionPane.showInputDialog(null, "Choose data structure to visualize:", "Data Structure Visualization", JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
-
-
-
-//            if (choice != null) {
-//                switch (choice) {
-//                    case "Stack":
-//                        new StackGui().setVisible(true);
-//                        break;
-//                    case "Queue":
-//                        new QueueGui().setVisible(true);
-//                        break;
-//                    case "LinkedList":
-//                        new LinkedListGui().setVisible(true);
-//                        break;
-//                    case "Deque":
-//                        new DequeGui().setVisible(true);
-//                        break;
-//                    default:
-//                        System.out.println("Invalid choice.");
-//                        break;
-//                }
-//            }
-//        });
     }
 }
